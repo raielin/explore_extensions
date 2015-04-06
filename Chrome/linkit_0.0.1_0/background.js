85e9229c4a0c2d4268afd26f5e8bbe9a9481b148
raielin@gmail.com <raielin@gmail.com>
2015-04-06 17:23:02 -0400

    Create background.js to establish browser action.

---
+++ Chrome/linkit_0.0.1_0/background.js
@@ -0,0 +1,19 @@
+// background.js
+// Send an arbitrary JSON payload to current tab. Chose "message" as key but could be anything.
+
+// Called when user clicks on browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send message to active tab
+  chrome.tabs.query(
+  {
+    active: true,
+    currentWindow: true
+  },
+
+  function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {
+      "message": "clicked_browser_action"
+    });
+  });
+});

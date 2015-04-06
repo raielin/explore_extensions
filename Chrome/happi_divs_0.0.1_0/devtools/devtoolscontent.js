65bdcb7c4f267bb1e9be4eb6ba96d46f8392ecaa
raielin@gmail.com <raielin@gmail.com>
2015-04-06 11:48:33 -0400

    Create devtoolscontent.js

---
+++ Chrome/happi_divs_0.0.1_0/devtools/devtoolscontent.js
@@ -0,0 +1,13 @@
+// devtools/devtoolscontent.js
+
+window.onload = function() {
+  var port = chrome.extension.connect({
+    name: "color-divs-port"
+  });
+
+  document.getElementById("button").onclick = function() {
+    port.postMessage({
+      type: "color-divs"
+    });
+  }
+}

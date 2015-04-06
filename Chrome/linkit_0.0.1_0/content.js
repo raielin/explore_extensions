91abb24c71e03220e1d8fac46512a268783e9a64
raielin@gmail.com <raielin@gmail.com>
2015-04-06 17:25:28 -0400

    Make content.js listen for response from browser action and pull URL from current page.

--- Chrome/linkit_0.0.1_0/content.js
+++ Chrome/linkit_0.0.1_0/content.js
@@ -5,6 +5,22 @@
 // alert("Hello from link-IT!")
 
 // Use jQuery to log first external link on a page.
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+// var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-alert("link-IT: " + firstHref);
+// alert("link-IT: " + firstHref);
+
+// Listen for message from background.js
+// only run code inside listener when payload is received.
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.message === "clicked_browser_action") {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      var activate = window.confirm("link-IT to " + firstHref + "?");
+      if (activate) {
+        console.log("link-IT!");
+      } else {
+        console.log("nada.");
+      }
+    }
+  }
+);

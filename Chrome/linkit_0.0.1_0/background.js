609a93e5a63293b1031e0aa6261d4213bb4a2b3c
raielin@gmail.com <raielin@gmail.com>
2015-04-06 18:11:18 -0400

    Make background.js listen for OK from content.js to create new tab with given URL.

--- Chrome/linkit_0.0.1_0/background.js
+++ Chrome/linkit_0.0.1_0/background.js
@@ -17,3 +17,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     });
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "open_new_tab") {
+      chrome.tabs.create({
+        "url": request.url
+      });
+    }
+  }
+);

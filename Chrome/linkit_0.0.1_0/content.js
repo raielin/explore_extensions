819785532b205f53d6ba6e9f297bd61813663600
raielin@gmail.com <raielin@gmail.com>
2015-04-06 16:37:05 -0400

    Use jQuery to log first external link on a page.

--- Chrome/linkit_0.0.1_0/content.js
+++ Chrome/linkit_0.0.1_0/content.js
@@ -1,3 +1,10 @@
 // content.js
+// NOTE: Do not need to check if document has loaded before running content script. Chrome default injects content scripts only after DOM is complete. (https://developer.chrome.com/extensions/content_scripts#run_at)
 
-alert("Hello from link-IT!")
+// Create simple alert to test extension
+// alert("Hello from link-IT!")
+
+// Use jQuery to log first external link on a page.
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+alert("link-IT: " + firstHref);

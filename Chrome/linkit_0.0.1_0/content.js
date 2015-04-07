74a38dc0929568641182f44992c026b932b4bd40
raielin@gmail.com <raielin@gmail.com>
2015-04-07 12:01:40 -0400

    Make better comments

--- Chrome/linkit_0.0.1_0/content.js
+++ Chrome/linkit_0.0.1_0/content.js
@@ -1,17 +1,20 @@
 // content.js
 // NOTE: Do not need to check if document has loaded before running content script. Chrome default injects content scripts only after DOM is complete. (https://developer.chrome.com/extensions/content_scripts#run_at)
 
-// Create simple alert to test extension
+// ======== VERSION 0.1 ==========
+// // Create simple alert to test extension
 // alert("Hello from link-IT!")
 
-// Use jQuery to log first external link on a page.
+// ======== VERSION 0.2 ==========
+// // Use jQuery to log first external link on a page.
 // var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 // alert("link-IT: " + firstHref);
 
-// Listen for message from background.js
-// only run code inside listener when payload is received.
-// simple version - no message being sent back to background.js
+// ======== VERSION 0.3 ==========
+// // Listen for message from background.js
+// // only run code inside listener when payload is received.
+// // simple version - no message being sent back to background.js
 // chrome.runtime.onMessage.addListener(
 //   function(request, sender, sendResponse) {
 //     if(request.message === "clicked_browser_action") {
@@ -26,6 +29,7 @@
 //   }
 // );
 
+// ======== VERSION 0.4 ==========
 // Listen for message from background.js that indicates we should pull the first URL from the current page
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {

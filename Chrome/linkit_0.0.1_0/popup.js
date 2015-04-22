997a77cda411ff9b7e573b29092a3c63b4963223
raielin@gmail.com <raielin@gmail.com>
2015-04-22 14:24:38 -0400

    Use jQuery "document ready" instead of window.onload.

--- Chrome/linkit_0.0.1_0/popup.js
+++ Chrome/linkit_0.0.1_0/popup.js
@@ -1,11 +1,11 @@
 'use strict';
 
-window.onload = function() {
+$(document).ready(function() {
   // alert('go link-IT!');  // for testing use only
   document.getElementById("button").onclick = function() {
     goLinkIt();
   };
-};
+});
 
 // send message to background.js to open selected url in new tab
 // using list of URLs as parameter here as a hack for now. Need to map through HTML elements instead and grab text value of selected table row.

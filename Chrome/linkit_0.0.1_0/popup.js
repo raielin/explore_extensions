46122cabcd9b7e76b5d82a51e810f3c2e4ad9474
raielin@gmail.com <raielin@gmail.com>
2015-04-29 15:31:04 -0400

    Do a console log at start of script for testing purposes. Add listener for message from content with list of URLs from DOM. Run function to populate popup with URLS upon receipt of message. Inject parameter into goLinkIt function call. Create blank function for populateLinksList().

--- Chrome/linkit_0.0.1_0/popup.js
+++ Chrome/linkit_0.0.1_0/popup.js
@@ -1,9 +1,9 @@
 'use strict';
 
 $(document).ready(function() {
+  // console.log("document ready!"); // for testing use only.
 
   var linksList;
-  // alert('go link-IT!');  // for testing use only
 
   // Find active tab in order to send a message to content script
   chrome.tabs.query(
@@ -21,10 +21,28 @@ $(document).ready(function() {
     }
   );
 
+// Listen for message from content that includes list of URLs to insert into popup view.
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if ((request.from === "content") && (request.message === "linksList_success")) {
+        linksList = request.linksList;
+        // console.log(request.linksList);  // for testing use only
+        populateLinksList(request.linksList);
+      }
+    }
+  );
+
   document.getElementById("button").onclick = function() {
-    goLinkIt();
+    goLinkIt(linksList);
   };
 });
+
+
+// Function to insert URLs into popup HTML.
+// Takes an array of URLs as a parameter, creates list of URLs in popup view.
+function populateLinksList(urls) {
+
+}
 
 // send message to background.js to open selected url in new tab
 // using list of URLs as parameter here as a hack for now. Need to map through HTML elements instead and grab text value of selected table row.

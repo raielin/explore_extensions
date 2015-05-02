6dcc524dd637c3f22025f7297858b31be591e12a
raielin@gmail.com <raielin@gmail.com>
2015-05-02 11:23:05 -0400

    Remove unnecessary variable assignments for active tab. Remove alert confirmation. Add location.reload() to update popup view with new added link.

--- Chrome/linkit_0.0.2_5/linkit_ext_app/popup_module/popup.js
+++ Chrome/linkit_0.0.2_5/linkit_ext_app/popup_module/popup.js
@@ -9,9 +9,7 @@ $(document).ready(function() {
       currentWindow: true
     },
     function(tabs) {
-      // var title = tabs[0].title;
-      // var url = tabs[0].url;
-      // console.log(title + ': ' + url);  // for testing use
+      // console.log(tabs[0].title + ': ' + tabs[0].url);  // for testing use
 
       chrome.extension.sendMessage({
         from: 'popup',
@@ -21,8 +19,9 @@ $(document).ready(function() {
           url: tabs[0].url
         }
       });
-
-      alert('Marked!');
+      // Reload popup after adding new link.
+      // location.reload() can take a parameter. Default is `false` which means the window reloads from the browser cache; true will reload window from server.
+      location.reload();
     });
   });
 });
